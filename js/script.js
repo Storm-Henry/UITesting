c6e45fa3185c706cc368682718b88f2948b1338d
Storm Henry <storm.henry@redlioncanada.com>
2016-12-15 16:32:20 -0500

    added pattern in the background

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+var snap = 10; /* the value of the snap in pixels */
+
   $( function() {
-    $( "#draggable" ).draggable();
+    $( "#draggable" ).draggable({ 
+    	grid: [ 10, 10 ]
+     });
+    $( "#draggable" ).draggable({
+ 	 containment: $("#container")
+});
+
+
   } );
